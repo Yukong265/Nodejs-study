38f0070fa70b3e5c1021d246aec68e84d574dba4
Yu Bin <subenmoom50@gmail.com>
2021-04-26 19:50:08 +0900

    delete tool implementation
    description delete tool

--- main.js
+++ main.js
@@ -61,7 +61,10 @@ var app = http.createServer(function (request, response) {
             `<h2>${title}</h2>${description}`,
             `<a href="/create">create</a>
             <a href="/update?id=${title}">update</a>
-            <a href="/delete?id=${title}">delete</a>`
+            <form action="delete_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <input type="submit" value="delete">
+            </form>`
           );
           response.writeHead(200);
           response.end(template);
@@ -100,9 +103,9 @@ var app = http.createServer(function (request, response) {
         response.end();
       })
     });
-  } else if(pathname === '/update'){
-    fs.readdir('./data', function(error, filelist){
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+  } else if (pathname === '/update') {
+    fs.readdir('./data', function (error, filelist) {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
         var title = queryData.id;
         var list = templateList(filelist);
         var template = templateHTML(title, list,
@@ -124,22 +127,35 @@ var app = http.createServer(function (request, response) {
         response.end(template);
       });
     });
-  } else if(pathname === '/update_process'){
+  } else if (pathname === '/update_process') {
     var body = '';
-    request.on('data', function(data){
-        body = body + data;
+    request.on('data', function (data) {
+      body = body + data;
     });
-    request.on('end', function(){
-        var post = qs.parse(body);
-        var id = post.id;
-        var title = post.title;
-        var description = post.description;
-        fs.rename(`data/${id}`, `data/${title}`, function(error){
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location:encodeURI(`/?id=${title}`)});
-            response.end();
-          })
-        });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function (error) {
+        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+          response.writeHead(302, { Location: encodeURI(`/?id=${title}`) });
+          response.end();
+        })
+      });
+    });
+  } else if (pathname === '/delete_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body = body + data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function (error) {
+        response.writeHead(302, { Location: encodeURI(`/`) });
+        response.end();
+      })
     });
   } else {
     response.writeHead(404);
