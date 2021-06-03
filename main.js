5fb572fccda4573af7aa765f8f1ff70189a587af
Yu Bin <subenmoom50@gmail.com>
2021-06-03 11:42:21 +0900

    rock scissors paper after login or register
    completed

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const rl = readline.createInterface({
 });
 
 var Rps = function () {
-
+    
 };
 
 var i = 0;
@@ -16,19 +16,25 @@ rl.question('Login : 1, Register : 2 \n', (answer) => {
             fs.readdir('./data/', function (err, filelist) {
                 fs.readFile(`data/${answer}`, 'utf-8', function (err, data) {
                     var ID = answer;
+                    var findingFile
                     while (i < filelist.length) {
                         if (ID === filelist[i]) {
-                            rl.question('PassWord : ', (answer) => {
-                                var PassWord = answer;
-                                if (PassWord === data) {
-                                    console.log('Login Success!');
-                                } else {
-                                    console.log('Incorrect Password!')
-                                }
-                            })
-                        } else
-                            console.log('Incorrect ID!');
+                            findingFile = filelist[i]
+                        }
                         i += 1;
+                    }
+                    if (findingFile === undefined) {
+                        console.log('Incorrect ID!');
+                    } else {
+                        rl.question('PassWord : ', (answer) => {
+                            var PassWord = answer;
+                            if (PassWord === data) {
+                                console.log('Login Success!');
+                                ques();
+                            } else {
+                                console.log('Incorrect Password!');
+                            }
+                        })
                     }
                 })
             })
@@ -45,3 +51,62 @@ rl.question('Login : 1, Register : 2 \n', (answer) => {
         });
     }
 })
+
+var result;
+function ques() {
+    rl.question('Rock Scissors Paper Game!\nYour Answer : ', (answer) => {
+        const random = Math.random()
+        if (random <= 0.33) {
+            result = 'Scissors';
+        }
+        else if (random <= 0.66) {
+            result = 'Paper';
+        }
+        else {
+            result = 'Rock';
+        }
+        RSP(answer, result);
+    })
+}
+function RSP(answer, result) {
+    if (answer === result) {
+        console.log('Draw!');
+        RSP(answer, result);
+    }
+    else if (answer === 'Scissors') {
+        if (result === 'Rock') {
+            console.log('Your answer : ' + answer);
+            console.log("Computer's answer : " + result);
+            console.log('You Defeat!');
+        }
+        else if (result === 'Paper') {
+            console.log('Your answer : ' + answer);
+            console.log("Computer's answer : " + result);
+            console.log('You Win!');
+        }
+    }
+    else if (answer === 'Rock') {
+        if (result === 'Scissors') {
+            console.log('Your answer : ' + answer);
+            console.log("Computer's answer : " + result);
+            console.log('You Win!');
+        }
+        else {
+            console.log('Your answer : ' + answer);
+            console.log("Computer's answer : " + result);
+            console.log('You Defeat!');
+        }
+    }
+    else if (answer === 'Paper') {
+        if (result === 'Scissors') {
+            console.log('Your answer : ' + answer);
+            console.log("Computer's answer : " + result);
+            console.log('You Defeat!');
+        }
+        else {
+            console.log('Your answer : ' + answer);
+            console.log("Computer's answer : " + result);
+            console.log('You Win!');
+        }
+    }
+}
--- test.js
+++ test.js
@@ -4,11 +4,60 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-var i=0;
-fs.readdir('./data', function(err, filelist){
-    while(i<filelist.length){
-        console.log(filelist[i]);
-        i += 1;
-        
+var result;
+function ques() {
+    rl.question('Rock Scissors Paper Game!\nYour Answer : ', (answer) => {
+        const random = Math.random()
+        if (random <= 0.33) {
+            result = 'Scissors';
+        }
+        else if (random <= 0.66) {
+            result = 'Paper';
+        }
+        else {
+            result = 'Rock';
+        }
+        RSP(answer, result);
+    })
+}
+function RSP(answer, result) {
+    if (answer === result) {
+        console.log('Draw!');
     }
-})
+    else if (answer === 'Scissors') {
+        if (result === 'Rock') {
+            console.log('Your answer : ' + answer);
+            console.log("Computer's answer : " + result);
+            console.log('Defeat!');
+        }
+        else if (result === 'Paper') {
+            console.log('Your answer : ' + answer);
+            console.log("Computer's answer : " + result);
+            console.log('Win!');
+        }
+    }
+    else if (answer === 'Rock') {
+        if (result === 'Scissors') {
+            console.log('Your answer : ' + answer);
+            console.log("Computer's answer : " + result);
+            console.log('Win!');
+        }
+        else {
+            console.log('Your answer : ' + answer);
+            console.log("Computer's answer : " + result);
+            console.log('Defeat!');
+        }
+    }
+    else if (answer === 'Paper') {
+        if (result === 'Scissors') {
+            console.log('Your answer : ' + answer);
+            console.log("Computer's answer : " + result);
+            console.log('Defeat!');
+        }
+        else {
+            console.log('Your answer : ' + answer);
+            console.log("Computer's answer : " + result);
+            console.log('Win!');
+        }
+    }
+}
