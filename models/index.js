4323e801fe00dfe63d197fda49a91568a6d655be
Yu Bin <subenmoom50@gmail.com>
2021-12-22 22:59:18 +0900

    add index.js

---
+++ models/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const Sequelize = require("sequelize");
+const Item = require("./item");
+const User = require("./user");
+
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config.json")[env];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = this.User;
+db.Item = this.Item;
+
+db.User = require("./user")(sequelize, Sequelize);
+db.Item = require("./item")(sequelize, Sequelize);
+
+db.User.hasMany(db.Item, { foreignKey: "username", targetKey: "username" });
+db.Item.belongsTo(db.User, { foreignKey: "username"});
+
+module.exports = db;
