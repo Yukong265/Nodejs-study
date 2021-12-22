66bfded6c750557d79a210b0e544ee6c19391fe5
Yu Bin <subenmoom50@gmail.com>
2021-12-22 22:59:07 +0900

    add config.js

---
+++ config/config.js
@@ -0,0 +1,27 @@
+const env = process.env;
+
+const development = {
+    username: env.SQLUSERANME
+}
+
+// "development": {
+//     "username": "root",
+//     "password": "rlawjdtjs0",
+//     "database": "tdl",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
