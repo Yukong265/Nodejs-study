901961d44b796976cec49b0a7411b8278996e10b
Yu Bin <subenmoom50@gmail.com>
2021-12-29 11:21:54 +0900

    add base

--- app.js
+++ app.js
@@ -64,6 +64,10 @@ app.use((err, req,res, next)=>{
     res.send('error');
 })
 
+app.get('/', (req,res)=>{
+    res.send('1231');
+})
+
 
 app.listen(app.get('port'), ()=>{
     console.log(app.get('port'), '번 포트에서 대기 중');
